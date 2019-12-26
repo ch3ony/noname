ba12f7479757124d3a449f6fb5b7b37ff909c50b
오성 김 <rladhtjdl@gc.gachon.ac.kr>
2019-12-26 17:45:10 +0900

    second commit

--- Info_util.py
+++ Info_util.py
@@ -6,5 +6,6 @@ class info_device:
             device_manager = frida.get_device_manager()
             device_list = device_manager.enumerate_devices()
             print(device_list)
+            print("testtesttest")
         except Exception as e:
             print(str(e))
